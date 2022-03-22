0d42f33dc65f7d9c1b326620d49177bfc7e021f3
igor <igorflribeiro@gmail.com>
2022-03-22 19:31:17 -0300

    playlist page partial layout

--- pages/playlist/[id].tsx
+++ pages/playlist/[id].tsx
@@ -1,9 +1,34 @@
 import prisma from "../../lib/prisma";
 import {validateToken} from "../../lib/auth";
+import GradientLayout from "../../components/gradientLayout";
+
+const getBGColor = id => {
+    const colors = [
+        'red',
+        'green',
+        'blue',
+        'yellow',
+        'orange',
+        'purple',
+        'gray',
+        'teal',
+    ]
+
+    return colors[id - 1] || colors[Math.floor(Math.random() * colors.length)]
+}
 
 const PLaylist = ({ playlist }) => {
+    const color = getBGColor(playlist.id)
+
     return (
         <>
+            <GradientLayout color={color} roundImage={false} title={playlist.name} subtitle="playlist"
+            description={`${playlist.songs.length} songs`} image="https://picsum.photos/200"
+            >
+            <div>
+                oi
+            </div>
+            </GradientLayout>
             <div>{playlist.id}</div>
             <div>{playlist.name}</div>
         </>
